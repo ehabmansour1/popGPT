06bd0eb79412de1825ab36756c72de252fcac290
Ehab Mansour <mansehab990@gmail.com>
2025-03-22 07:19:17 +0200

    chat edit

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./login";
+import Register from "./register";
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
